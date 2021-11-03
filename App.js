92ee0e6f50251b0ac644d72296954f2bda01a47c
Yamete-ymt <ymt.yemyattun@gmail.com>
2021-11-03 16:49:46 +0630

    commit 2

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import MainDrawer from "./Navigators/mainDrawer";
 import { NavigationContainer } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { LogBox } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App(){
   LogBox.ignoreLogs(['Reanimated 2']);  //to hide reanimated 2 installation warning
   useEffect(()=>{
     async function resetter(){
+      SplashScreen.hide();
       if(await AsyncStorage.getItem("@SetDone") != "true")
       {
         await AsyncStorage.setItem("@Person1Name", "Person 1")
